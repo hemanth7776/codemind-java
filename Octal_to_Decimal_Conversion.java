fde54ca52b401c24312989e556ed499611a11047
hemanth7776 <115137139+hemanth7776@users.noreply.github.com>
2024-01-29 15:34:30 +0530

    import java.util.*; public class Decimal {     public static void main(String[] args)     {         Scanner read=new Scanner (System.in);         String x=read.next();
                int d=Integer.parseInt(x,8);
                System.out.println(d);
            
        }
    }

---
+++ Octal_to_Decimal_Conversion.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Decimal
+{
+    public static void main(String[] args)
+    {
+        Scanner read=new Scanner (System.in);
+        String x=read.next();
+        
+        
+           
+            int d=Integer.parseInt(x,8);
+            System.out.println(d);
+        
+    }
+}
