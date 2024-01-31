acc4dc1b0132b7d8db931fc3623ff6ecc70c9169
hemanth7776 <115137139+hemanth7776@users.noreply.github.com>
2024-01-31 10:06:17 +0530

    import java.util.*; public class Sedign {     public static void main(String[] args)     {         Scanner read=new Scanner(System.in);         int x1=read.nextInt();         int y1=read.nextInt();         int x2=read.nextInt();         int y2=read.nextInt();         int a=x1+y1;         int b=x2+y2;         if(a>b)
            System.out.print(b);
            else
            System.out.print(a);
            
        }
    }

---
+++ Interior_Design.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Sedign
+{
+    public static void main(String[] args)
+    {
+        Scanner read=new Scanner(System.in);
+        int x1=read.nextInt();
+        int y1=read.nextInt();
+        int x2=read.nextInt();
+        int y2=read.nextInt();
+        int a=x1+y1;
+        int b=x2+y2;
+        if(a>b)
+        
+        System.out.print(b);
+        else
+        System.out.print(a);
+        
+    }
+}
