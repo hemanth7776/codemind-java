bede8fbf6aef892be3da15fac8ec609683430bf2
hemanth7776 <115137139+hemanth7776@users.noreply.github.com>
2024-02-06 10:08:55 +0530

    import java.util.*; public class Romeo {     public static void main(String[] args)     {         Scanner read=new Scanner(System.in);         int x=read.nextInt();         int y=read.nextInt();         int z=read.nextInt();         System.out.print(((x*5)+(y*10))/z);     } }

---
+++ Romeo_and_Juliet.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Romeo
+{
+    public static void main(String[] args)
+    {
+        Scanner read=new Scanner(System.in);
+        int x=read.nextInt();
+        int y=read.nextInt();
+        int z=read.nextInt();
+        System.out.print(((x*5)+(y*10))/z);
+    }
+}
